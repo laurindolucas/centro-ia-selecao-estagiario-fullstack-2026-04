449cc8c52496263d08fc99eb9319a9263ae31aaa
laurindolucas <caiolucax2024@gmail.com>
2026-04-12 11:26:04 -0300

    feat: criar models incial

---
+++ uniCarona/backend/models/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import (Column, Integer, String, ForeignKey)
+from database.connection import Base
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(120), nullable=False)
+    tipo = Column(String(120), nullable=False)
+    descricao = Column(String(250))
+    email = Column(String, String(120), unique=True, nullable=False)
+    senha_hash = Column(String(255), nullable=False)
+    
+    
+class Rota(Base):
+    __tablename__ = "rotas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+    origem = Column(String(120), nullable=False)
+    destino = Column(String(120), nullable=False)
+    horario = Column(String(20), nullable=False)
